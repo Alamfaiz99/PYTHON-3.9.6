bce64ad9581c4d73ce74f2cfe9ec73ec4f355d30
sm faizan alam <faizanalam986@gmail.com>
2021-12-30 12:49:06 +0530

    using enumerate

---
+++ enumerate.py
@@ -0,0 +1,9 @@
+#enumerate-
+#if you want indexno. of iterable data types
+#we use enumerate
+#iterable built-in datatypes-list,tuple,set,dict,str
+#now see how we use enumerate
+l=[10,20,30,40,50]
+for index,value in enumerate(l):
+    print(index,0value)
+    
